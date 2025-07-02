5404a6cd94f6411218271e5196cccba2c03da440
Bradley Bossard <bradleybossard@gmail.com>
2025-07-02 12:45:30 -0700

    adding basic map to renderer

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import * as THREE from "three";
 import { Renderer } from "./components/Renderer";
 import { Camera } from "./components/Camera";
 import { player } from "./components/Player";
+import { map, initializeMap } from "./components/Map";
+
 import "./style.css";
 
 const scene = new THREE.Scene();
 scene.add(player);
+scene.add(map);
+initializeMap();
+
 
 const ambientLight = new THREE.AmbientLight();
 scene.add(ambientLight);
